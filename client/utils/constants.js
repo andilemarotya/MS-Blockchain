247bd3cb567b035c3834a9c268c21440ec6a990f
Mandile Andile Marotya <marotyamandile@gmail.com>
2023-03-24 12:47:55 +0200

    smart contract done

---
+++ client/utils/constants.js
@@ -0,0 +1,5 @@
+import abi from './Transactions.json';
+
+export const contractABI = abi.abi;
+export const contractAddress = '0x2dE834E4c8B8b6fB70Dc8759A643b55843E5C4C0';
+
--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,6 +1,13 @@
-require("@nomicfoundation/hardhat-toolbox");
+// https://eth-sepolia.g.alchemy.com/v2/-_o6tc_KAChP62j0TJHnpz3BeCAxe_U8
 
-/** @type import('hardhat/config').HardhatUserConfig */
+require(`@nomiclabs/hardhat-waffle`);
+
 module.exports = {
-  solidity: "0.8.18",
-};
+  solidity: '0.8.0',
+  networks: {
+    sepolia: {
+      url: "https:eth-sepolia.g.alchemy.com/v2/-_o6tc_KAChP62j0TJHnpz3BeCAxe_U8",
+      accounts: ['8db0e69db3065a66bef59b02c04b24a31130e34c3eaf7a50ffa8946cb17a1b58']
+    }
+  }
+}
