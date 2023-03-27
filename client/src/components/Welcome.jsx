c61ed0cfe26ad3e8b740cb863b3e5e1e5ac36007
Mandile Andile Marotya <marotyamandile@gmail.com>
2023-03-27 16:22:18 +0200

    correct arrangement

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,19 +1,18 @@
+import React, {useContext} from "react";
 import { AiFillPlayCircle } from "react-icons/ai";
 import { SiEthereum } from 'react-icons/si';
 import { BsInfoCircle } from 'react-icons/bs';
 
+import {TransactionContext} from '../context/TransactionContext';
 import { Loader } from './';
 import { useState } from "react";
-
 
 
 const commonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
 
 const Welcome = () => {
 
-    const connectWallet = () => {
-
-    }
+    const { connectWallet } = useContext(TransactionContext); // Transffering the data to help us connect to the blockchain
 
     const handleSubmit = () => {
 
@@ -46,7 +45,7 @@ const Welcome = () => {
                     </p>
                     <button
                         type="button"
-                        onClick={connectWallet}
+                        onClick={ connectWallet }
                         className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]" 
                     >
                         <p className="text-white text-base font-semibold">Connect Wallet</p>
